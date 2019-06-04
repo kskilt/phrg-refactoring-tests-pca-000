842dc03faf98835346d46b6a29732900e2853eb7
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-06-04 16:25:20 -0400

    done

---
+++ spec/factories/author.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :author do
+    name "Factory McFactorFace"
+    phone_number "1234567890"
+  end
+end
---
+++ spec/factories/post.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post do
+    title "You Won't Believe These True Facts"
+    content "So this is a paragraph, and it describes things. Sometimes it contains run on sentences that seem to never end but thats really because we need to hit a certain character length to be uniform with the validations. Besides that, its really not that bad, and quite commical. I mean there is a few grammatical errors, but it is a string so who cares?!?!?"
+    summary "Summary summarizing a summarial portion of sum"
+    category "Non-Fiction"
+  end
+end
